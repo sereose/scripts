fe2b3c04343a9cebd540121ad5802b2616ef7c91
sereose <churilov@gmail.com>
2020-09-14 14:27:00 +0300

    Create install_zsh.sh

---
+++ install_zsh.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+echo "installing latest oh my zsh"
+
+sudo apt-get install zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+chsh -s /bin/zsh
